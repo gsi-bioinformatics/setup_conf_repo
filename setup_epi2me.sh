27373bcb30bd9bd3b216962c008edb1a41d4ca97
gsi-bioinformatics <121219192+gsi-bioinformatics@users.noreply.github.com>
2024-09-10 10:11:00 +0700

    update setup_epi2me.sh

--- setup_epi2me.sh
+++ setup_epi2me.sh
@@ -11,12 +11,9 @@ wget https://ont-exd-int-s3-euwst1-epi2me-labs.s3.amazonaws.com/installers/epi2m
 sudo dpkg -i epi2me_5.1.14_amd64.deb
 
 mkdir ~/Data
-
-sudo apt install unzip
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-aws configure
+cd Data
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.fai
 
 
 sudo reboot
